0d1f16254d4d70fa016ca55c41f968a18d8994d2
tleister <tleister@student.42heilbronn.de>
2025-04-21 15:10:58 +0200

    moved all structs in structs.h

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: tleister <tleister@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 15:57:41 by mpoplow           #+#    #+#             */
-/*   Updated: 2025/04/21 15:03:40 by tleister         ###   ########.fr       */
+/*   Updated: 2025/04/21 15:09:49 by tleister         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 # include "../libft/libft.h"
 # include "MLX42.h"
+# include "structs.h"
 # include <math.h>
 # include <stdbool.h>
 # include <stdio.h>
@@ -28,105 +29,12 @@
 # define WIDTH 1500
 # define HEIGTH 900
 
-// ~-~-~-~-~-~-~  Definitions and structs  ~-~-~-~-~-~-~ //
-
-typedef struct s_coord
-{
-	double			x;
-	double			y;
-	double			z;
-}					t_coords;
-
-typedef struct s_rgb
-{
-	unsigned char	r;
-	unsigned char	g;
-	unsigned char	b;
-}					t_rgbcolor;
-
-// ~-~-~-~-~-~-~ Scene elements ~-~-~-~-~-~-~ //
-
-typedef struct s_data
-{
-	mlx_t			*mlx;
-	mlx_image_t		*img;
-}					t_data;
-typedef struct s_ambient
-{
-	double			amb_light;
-	t_rgbcolor		rgb;
-
-}					t_a;
-
-typedef struct s_camera
-{
-	t_coords		pos;
-	t_coords		vec;
-	int				fov;
-}					t_c;
-
-typedef struct s_light
-{
-	t_coords		pos;
-	double			bright;
-	t_rgbcolor		rgb;
-}					t_l;
-
-typedef struct s_sphere
-{
-	t_coords		pos;
-	double			dia;
-	t_rgbcolor		rgb;
-}					t_sp;
-
-typedef struct s_plane
-{
-	t_coords		pos;
-	t_coords		nvec;
-	t_rgbcolor		rgb;
-}					t_pl;
-
-typedef struct s_cylinder
-{
-	t_coords		pos;
-	t_coords		vec;
-	double			dia;
-	double			h;
-	t_rgbcolor		rgb;
-
-}					t_cy;
-
-typedef enum e_status
-{
-	amb,
-	cam,
-	light,
-	sphere,
-	plane,
-	cylinder
-}					t_status;
-
-typedef struct s_all
-{
-	t_status		type;
-	struct s_all	*next;
-	union
-	{
-		t_a			amb;
-		t_c			cam;
-		t_l			light;
-		t_sp		sphere;
-		t_pl		plane;
-		t_cy		cylinder;
-	};
-}					t_all;
-
 // ~-~-~-~-~-~-~  Functions  ~-~-~-~-~-~-~ //
 
-bool				ft_argvcheck(int argc, char **argv);
-bool				ft_init(t_data *data);
-void				keyfunc(mlx_key_data_t keydata, void *param);
-bool				ft_argvcheck(int argc, char **argv);
-void				ft_normalize_vector(t_coords *vec);
+bool	ft_argvcheck(int argc, char **argv);
+bool	ft_init(t_data *data);
+void	keyfunc(mlx_key_data_t keydata, void *param);
+bool	ft_argvcheck(int argc, char **argv);
+void	ft_normalize_vector(t_coords *vec);
 
 #endif
